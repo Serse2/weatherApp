97b0430671c0fa125a73f951e8acbd461d51a118
Sergio <sergio.berga@hotmail.it>
2019-10-05 12:13:59 +0200

    add refresh data and update new icon

--- weather.js
+++ weather.js
@@ -4,13 +4,11 @@ const showDate = document.querySelector('#date')
 const body = document.querySelector('body')
 const input = document.querySelector('input[type="text"]')
 
-//change background and color text
-let date = new Date();
-let getHours = date.getHours()
-let getMinute = date.getMinutes()
-let totalDate = getHours + ':' +getMinute
 
+//change background and color text from hour
 function chengeBackground(){
+  let date = new Date();
+  let getHours = date.getHours()
   if (getHours > 7 && getHours < 16){
     //imposta il background del colore mattino e color dark
     body.style.background = 'var(--morning)'
@@ -22,30 +20,38 @@ function chengeBackground(){
     body.style.color = 'var(--dark)'
     input.style.color = 'var(--dark)'
   }else{
-    //impostail background del colore in night e color light
+    //imposta il background del colore in night e color light
     body.style.background = 'var(--night)'
     body.style.color = 'var(--light)'
     input.style.color = 'var(--light)'
   }
-  
 }
+setInterval(chengeBackground(), 1000)
 
-chengeBackground()
-
-
+function refreshDate(){
+  let date = new Date();
+  let getHours = date.getHours()
+  let getMinute = date.getMinutes()
+  let totalDate = getHours + ':' + getMinute
+  return totalDate
+}
 // show weather 
 function showWeather(data){
-  result.classList.remove('sole','nuvoloso')
+  
+  result.classList.remove('sole','nuvoloso','piovoso')
   if (data.weather[0].description == 'cielo sereno' || data.weather[0].description == 'clear sky' || data.weather[0].description == 'ciel dégagé'){
     result.classList.add('icon','sole')
   }
   if (data.weather[0].description == 'poche nuvole' || data.weather[0].description == 'few clouds' || data.weather[0].description == 'peu nuageux' || data.weather[0].description == 'nubi sparse'  ){
     result.classList.add('icon','nuvoloso')
   }
-  document.querySelector('#temperature').innerText = `gradi: ${data.main.temp}°`
+  if (data.weather[0].description == 'pioggia leggera'){
+    result.classList.add('icon','piovoso')
+  }
+  document.querySelector('#temperature').innerText = `${Math.round(data.main.temp)}°`
   document.querySelector('#humidity').innerText = `umidità: ${data.main.humidity}%`
   document.querySelector('#weather').innerText = data.weather[0].description
-  showDate.innerText = 'ora: ' + totalDate
+  showDate.innerText = 'ora: ' + refreshDate()
 }
 
 async function getData(){
@@ -55,8 +61,11 @@ async function getData(){
   let apikey = '&APIKEY=b9f269bc4ef38fa12c192b0fd1f1a897'
   let apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&lang=${language}${apikey}`
   let response = await fetch(apiWeather).then(data => data.json())
-
   showWeather(response)
 }
 
+
 submit.addEventListener('click', getData)
+
+//implementazioni
+// 1- una volta selezionata 
