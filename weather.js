9014a624ab8621630ddeda33f872ff0f6dabf72e
Sergio <sergio.berga@hotmail.it>
2019-10-07 23:16:39 +0200

    Geolocation on load

--- weather.js
+++ weather.js
@@ -1,8 +1,25 @@
-const submit = document.querySelector('#submit')
-const result = document.querySelector('#result')
-const showDate = document.querySelector('#date')
-const body = document.querySelector('body')
-const input = document.querySelector('input[type="text"]')
+//global 
+
+const $ = document.querySelector
+
+
+const submit = $('#submit')
+const result = $('#result')
+const showDate = $('#date')
+const body = $('body')
+const input = $('input[type="text"]')
+
+
+navigator.geolocation.getCurrentPosition(async (position) => {
+  if (position) {
+    const response = await execFetch(`lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
+    if (!response.name){
+      $('.main').classList.add('error')
+    }
+    $('#country').value = response.name
+    showWeather(response)
+  }
+});
 
 
 //change background and color text from hour
@@ -10,12 +27,12 @@ function chengeBackground(){
   let date = new Date();
   let getHours = date.getHours()
   console.log(getHours)
-  if (getHours > 7 && getHours < 16){
+  if (getHours >= 7 && getHours <= 15){
     //imposta il background del colore mattino e color dark
     body.style.background = 'var(--morning)'
     body.style.color = 'var(--dark)'
     input.style.color = 'var(--dark)'
-  }else if (getHours > 17 && getHours < 20){
+  }else if (getHours >= 16 && getHours <= 20){
     //imposta il background del colore in afternoon e color light
     body.style.background = 'var(--afternoon)'
     body.style.color = 'var(--light)'
@@ -28,7 +45,7 @@ function chengeBackground(){
   }
 }
 chengeBackground()
-setInterval(chengeBackground, 1000 * 60 * 60)
+setInterval(chengeBackground, 3600000)
 
 function refreshDate(){
   let date = new Date();
@@ -39,40 +56,62 @@ function refreshDate(){
 }
 // show weather 
 function showWeather(data){
-  result.classList.remove('icon','sole','nuvoloso','piovoso')
+
+  const mapIconClass = {
+    'cielo sereno': 'sole',
+    'poche nuvole': 'soleggiato',
+    'nubi sparse': 'nuvoloso',
+    'pioggia leggera': 'piovoso',
+    'pioggerella': 'piovoso'
+  }
+
+
+  result.classList = ''
+  if (!data.weather || !data.weather.length) { return }
   let descriptionWeather = data.weather[0].description
-  if (descriptionWeather == 'cielo sereno'){
-    result.classList.add('icon','sole')
-  }
-  if (descriptionWeather == 'poche nuvole'){
-    result.classList.add('icon','soleggiato')
-  }
-  if (descriptionWeather == 'nubi sparse'){
-    result.classList.add('icon','nuvoloso')
-  }
-  if (descriptionWeather == 'pioggia leggera'){
-    result.classList.add('icon','piovoso')
-  }
-  document.querySelector('#temperature').innerText = `${Math.round(data.main.temp)}°`
-  document.querySelector('#humidity').innerText = `umidità: ${data.main.humidity}%`
-  document.querySelector('#weather').innerText = descriptionWeather
+  result.classList.add('icon', mapIconClass[descriptionWeather])
+
+  const sunset = new Date(data.sys.sunset * 1000)
+  result.classList.add((sunset < new Date() ? 'pm' : ''))
+
+
+  // Info
+  $('#temperature').innerText = `${Math.round(data.main.temp)}°`
+  $('#humidity').innerText = `umidità: ${data.main.humidity}%`
+  $('#weather').innerText = descriptionWeather
   showDate.innerText = 'ora: ' + refreshDate()
 }
+
+function initContent(){
+  $('#temperature').innerText = ''
+  $('#humidity').innerText = ''
+  $('#weather').innerText = ''
+  showDate.innerText = ''
+  $('.main').classList.remove('error')
+}
+
 //chiamata fetch per recupero dei dati
 async function getData(){
   //ad ogni chiamata resetta i risultati
-  document.querySelector('#temperature').innerText = ''
-  document.querySelector('#humidity').innerText = ''
-  document.querySelector('#weather').innerText = ''
-  showDate.innerText = ''
+  initContent()
   //chiamata fetch
-  let city = document.querySelector('#country').value
-  let units = 'metric'
-  let language =  'it'    //document.querySelector('#language').value
-  let apikey = '&APIKEY=b9f269bc4ef38fa12c192b0fd1f1a897'
-  let apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&lang=${language}${apikey}`
-  let response = await fetch(apiWeather).then(data => data.json())
+  let city = $('#country').value
+  const response = await execFetch(`q=${city}`)
+  if (!response.name){
+    $('.main').classList.add('error')
+  } 
   showWeather(response)
+}
+
+async function execFetch(query) {
+  let units = 'metric'
+  let language =  'it'    //$('#language').value
+  let apikey = '&APIKEY=b9f269bc4ef38fa12c192b0fd1f1a897'
+  let apiWeather = `https://api.openweathermap.org/data/2.5/weather?${query}&units=${units}&lang=${language}${apikey}`
+  let response = await fetch(apiWeather)
+  response = await response.json()
+
+  return response
 }
 submit.addEventListener('click', getData)
 
